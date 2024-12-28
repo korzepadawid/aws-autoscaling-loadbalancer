64c055118c855c01f9e36f4ae216693be28c1c6e
korzepadawid <korzepadawid@yahoo.com>
2024-12-28 17:38:03 +0100

    creating launch templates

--- main.go
+++ main.go
@@ -1,15 +1,63 @@
 package main
 
 import (
+	"context"
+	"encoding/base64"
 	"log"
+	"os"
+	"time"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
+)
+
+const (
+	ENV_FILE_PATH    = ".env"
+	USER_DATA_SCRIPT = "user_data.sh"
+
+	AWS_REGION                 = "us-east-1"
+	AWS_AMI_ID                 = "ami-01816d07b1128cd2d" // Amazon Linux 2023 AMI
+	AWS_LAUNCH_TEMPLATE_PREFIX = "webservice-launch-template-"
 )
 
 func main() {
 	logger := log.Default()
-	if err := godotenv.Load(".env"); err != nil {
+	if err := godotenv.Load(ENV_FILE_PATH); err != nil {
 		logger.Fatalf("Error loading .env file: %v", err)
 	}
 	logger.Println("Environment variables loaded successfully")
+
+	userDataBytes, err := os.ReadFile(USER_DATA_SCRIPT)
+	if err != nil {
+		logger.Fatalf("Error reading user_data.sh file: %v", err)
+	}
+	logger.Println("user_data.sh file read successfully")
+
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancelFunc()
+
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithDefaultRegion(AWS_REGION))
+	if err != nil {
+		log.Fatal(err)
+	}
+	logger.Println("AWS configuration loaded successfully")
+
+	ec2Client := ec2.NewFromConfig(cfg)
+	base64UserData := base64.StdEncoding.EncodeToString(userDataBytes)
+	ec2LaunchTemplate, err := ec2Client.CreateLaunchTemplate(ctx, &ec2.CreateLaunchTemplateInput{
+		LaunchTemplateData: &types.RequestLaunchTemplateData{
+			UserData:     aws.String(base64UserData),
+			ImageId:      aws.String(AWS_AMI_ID),
+			InstanceType: types.InstanceTypeT2Micro,
+		},
+		LaunchTemplateName: aws.String(AWS_LAUNCH_TEMPLATE_PREFIX + uuid.NewString()),
+	})
+	if err != nil {
+		logger.Fatalf("Error creating launch template: %v", err)
+	}
+	logger.Printf("Launch template created with ID: %s", *ec2LaunchTemplate.LaunchTemplate.LaunchTemplateId)
 }
---
+++ user_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/korzepadawid/aws-autoscaling-pzc/a9b3e70b8f76a0c4ab6d97120858dd6b5e51e066/webservice
+chmod +x webservice
+./webservice
